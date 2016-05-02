5856653a0b024f9c91167d4ec3649ebaf5bec9b8
Zeeshan Abid <zeesh.rocky@gmail.com>
2016-05-03 01:40:56 +0530

    ACD_JAVAB_Session_1_Assingment3
    Correct usage of data types

---
+++ Assignment3/src/Examples.java
@@ -0,0 +1,19 @@
+
+public class Examples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2;
+		String b="a";
+		int c=a+b; //String to int
+		
+		System.out.println("c is "+c);
+		
+		int x=1;
+		boolean y=true;
+		int z=(x+y); //Boolean to int
+		
+		System.out.println("z is "+z);
+	}
+
+}
